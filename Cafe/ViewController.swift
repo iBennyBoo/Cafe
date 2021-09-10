e1e29b7194ae76d902c9eb2b0147c0288588ae24
Ben Berman <bpb062004@gmail.com>
2021-09-10 07:46:58 -0500

    Initial Commit

---
+++ Cafe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cafe
+//
+//  Created by Benjamin Berman on 9/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
